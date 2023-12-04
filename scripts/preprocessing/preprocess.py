bc10f75e048a02548a37b0b58de70e00a57a987d
lewis trudeau <lewistrudeau@gmail.com>
2023-12-04 16:47:38 +0000

    Putting Joels preprocessing pipeline into a function inside Preprocess

--- scripts/preprocessing/preprocess.py
+++ scripts/preprocessing/preprocess.py
@@ -1,12 +1,13 @@
-# Data manipulation
 import pandas as pd
 import numpy as np
-
-# ML Processes
-from sklearn.preprocessing import OneHotEncoder, StandardScaler
-from sklearn.pipeline import Pipeline, FunctionTransformer
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.pipeline import Pipeline
+from sklearn.compose import make_column_transformer
 from sklearn.compose import ColumnTransformer
-
+from sklearn.decomposition import PCA
+from sklearn.linear_model import LinearRegression
 # External function
 from cleaner import *
 from encoder import *
@@ -26,3 +27,18 @@ def preprocessing(df, unique_vals):
 def data_cleaning(df):
     df_clean = cleaner(df)
     return df_clean
+
+def preprocessing_pipe(df):
+    num_transformer = MinMaxScaler()
+
+    cat_transformer = OneHotEncoder(handle_unknown='ignore', sparse_output = False)
+
+    transformer = make_column_transformer((num_transformer, ['Lat_OR','Lon_OR', 'Lat_DES','Lon_DES']),
+                                  (cat_transformer, ['ENGLISH_DAY_TYPE', 'SERVICE_GROUP_CODE_AFFECTED', 'INCIDENT_REASON',
+                                                        'UNIT_CLASS_AFFECTED', 'TRAIN_SERVICE_CODE_AFFECTED',
+                                                     'PERFORMANCE_EVENT_CODE',
+                                                     'APP_TIMETABLE_FLAG_AFF']),
+                                remainder = 'passthrough')
+
+    pipe = Pipeline([('transformer', transformer)])
+    return pipe
